2d2e622a8e630b77f535828ea1db6f4f3198d249
mitsuki <e165732@ie.u-ryukyu.ac.jp>
2016-12-13 01:22:09 +0900

    ステップ１終了

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by e165732 on 2016/12/13.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+}
