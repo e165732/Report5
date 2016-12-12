d183072f24cc235ed63482b4c21fe8026497bf45
mitsuki <e165732@ie.u-ryukyu.ac.jp>
2016-12-13 01:38:44 +0900

    ステップ２終了

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@
  */
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch(NullPointerException n){
+            System.out.println("NullPointerExceptionが発生した。");
+        }
     }
 }
